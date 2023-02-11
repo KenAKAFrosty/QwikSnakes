35029b3c5576ffcd07c11f9947e433dfd26e2ccb
FrostyyPaws <90424167+KenAKAFrosty@users.noreply.github.com>
2023-02-11 17:57:14 -0500

    basic move/mechanics in, time to try surviving

--- src/game-engine/functions.spec.ts
+++ src/game-engine/functions.spec.ts
@@ -1,9 +1,281 @@
-import {describe, expect, test} from "vitest"
-import { add } from "./functions"
+import { describe, expect, test } from "vitest"
+import { moveSnake, resolveBoardAndGetSnakeStatuses } from "./functions"
 
 
-describe("Game engine", ()=> { 
-    test("add", () => { 
-        expect(add(1, 2)).toBe(3)
+describe("Game engine", () => {
+    test("resolve board basic life", () => {
+        expect(resolveBoardAndGetSnakeStatuses({
+            height: 11,
+            width: 11,
+            snakes: [
+                {
+                    id: 'gs_yjxcD4dGd9yVF6ycGW6bW8gb',
+                    name: 'The Snakening Continues',
+                    latency: '128',
+                    health: 92,
+                    body: [{ x: 9, y: 9 }, { x: 9, y: 8 }, { x: 9, y: 7 }],
+                    head: { x: 9, y: 9 },
+                    length: 3,
+                    shout: 'AHHHHHHHH I\'M A SNAKE',
+                    squad: '',
+                    customizations: { color: '#ac7ef4', head: 'beluga', tail: 'mouse' }
+                },
+                {
+                    id: 'gs_x8HRCtPFT7cHFwwhMWQGWTW4',
+                    name: 'Hungry Bot',
+                    latency: '1',
+                    health: 94,
+                    body: [{ x: 4, y: 4 }, { x: 4, y: 3 }, { x: 3, y: 3 }, { x: 2, y: 3 }],
+                    head: { x: 4, y: 4 },
+                    length: 4,
+                    shout: '',
+                    squad: '',
+                    customizations: { color: '#00cc00', head: 'alligator', tail: 'alligator' }
+                }
+            ],
+            food: [{ x: 10, y: 2 }, { x: 5, y: 5 }, { x: 3, y: 2 }],
+            hazards: []
+        }
+        )).toEqual({
+            'gs_yjxcD4dGd9yVF6ycGW6bW8gb': { alive: true },
+            'gs_x8HRCtPFT7cHFwwhMWQGWTW4': { alive: true }
+        })
+    });
+
+    test("resolve board basic out of bounds death", () => {
+        expect(resolveBoardAndGetSnakeStatuses({
+            height: 11,
+            width: 11,
+            snakes: [
+                {
+                    id: 'gs_yjxcD4dGd9yVF6ycGW6bW8gb',
+                    name: 'The Snakening Continues',
+                    latency: '128',
+                    health: 92,
+                    body: [{ x: 9, y: 11 }, { x: 9, y: 10 }, { x: 9, y: 9 }],
+                    head: { x: 9, y: 9 },
+                    length: 3,
+                    shout: 'AHHHHHHHH I\'M A SNAKE',
+                    squad: '',
+                    customizations: { color: '#ac7ef4', head: 'beluga', tail: 'mouse' }
+                },
+                {
+                    id: 'gs_x8HRCtPFT7cHFwwhMWQGWTW4',
+                    name: 'Hungry Bot',
+                    latency: '1',
+                    health: 94,
+                    body: [{ x: 4, y: 4 }, { x: 4, y: 3 }, { x: 3, y: 3 }, { x: 2, y: 3 }],
+                    head: { x: 4, y: 4 },
+                    length: 4,
+                    shout: '',
+                    squad: '',
+                    customizations: { color: '#00cc00', head: 'alligator', tail: 'alligator' }
+                }
+            ],
+            food: [{ x: 10, y: 2 }, { x: 5, y: 5 }, { x: 3, y: 2 }],
+            hazards: []
+        }
+        )).toEqual({
+            'gs_yjxcD4dGd9yVF6ycGW6bW8gb': { alive: false },
+            'gs_x8HRCtPFT7cHFwwhMWQGWTW4': { alive: true }
+        })
+
+        expect(resolveBoardAndGetSnakeStatuses({
+            height: 11,
+            width: 11,
+            snakes: [
+                {
+                    id: 'gs_yjxcD4dGd9yVF6ycGW6bW8gb',
+                    name: 'The Snakening Continues',
+                    latency: '128',
+                    health: 92,
+                    body: [{ x: 9, y: 11 }, { x: 9, y: 10 }, { x: 9, y: 9 }],
+                    head: { x: 9, y: 9 },
+                    length: 3,
+                    shout: 'AHHHHHHHH I\'M A SNAKE',
+                    squad: '',
+                    customizations: { color: '#ac7ef4', head: 'beluga', tail: 'mouse' }
+                },
+                {
+                    id: 'gs_x8HRCtPFT7cHFwwhMWQGWTW4',
+                    name: 'Hungry Bot',
+                    latency: '1',
+                    health: 94,
+                    body: [{ x: 4, y: -1 }, { x: 4, y: 0 }, { x: 4, y: 1 }, { x: 4, y: 2 }],
+                    head: { x: 4, y: 4 },
+                    length: 4,
+                    shout: '',
+                    squad: '',
+                    customizations: { color: '#00cc00', head: 'alligator', tail: 'alligator' }
+                }
+            ],
+            food: [{ x: 10, y: 2 }, { x: 5, y: 5 }, { x: 3, y: 2 }],
+            hazards: []
+        }
+        )).toEqual({
+            'gs_yjxcD4dGd9yVF6ycGW6bW8gb': { alive: false },
+            'gs_x8HRCtPFT7cHFwwhMWQGWTW4': { alive: false }
+        })
+    });
+
+    test("resolve board health loss / food consumption", () => {
+        expect(resolveBoardAndGetSnakeStatuses({
+            height: 11,
+            width: 11,
+            snakes: [
+                {
+                    id: 'gs_yjxcD4dGd9yVF6ycGW6bW8gb',
+                    name: 'The Snakening Continues',
+                    latency: '128',
+                    health: 92,
+                    body: [{ x: 9, y: 9 }, { x: 9, y: 8 }, { x: 9, y: 7 }],
+                    head: { x: 9, y: 9 },
+                    length: 3,
+                    shout: 'AHHHHHHHH I\'M A SNAKE',
+                    squad: '',
+                    customizations: { color: '#ac7ef4', head: 'beluga', tail: 'mouse' }
+                },
+                {
+                    id: 'gs_x8HRCtPFT7cHFwwhMWQGWTW4',
+                    name: 'Hungry Bot',
+                    latency: '1',
+                    health: 1,
+                    body: [{ x: 4, y: 4 }, { x: 4, y: 3 }, { x: 3, y: 3 }, { x: 2, y: 3 }],
+                    head: { x: 4, y: 4 },
+                    length: 4,
+                    shout: '',
+                    squad: '',
+                    customizations: { color: '#00cc00', head: 'alligator', tail: 'alligator' }
+                }
+            ],
+            food: [{ x: 10, y: 2 }, { x: 5, y: 5 }, { x: 3, y: 2 }],
+            hazards: []
+        }
+        )).toEqual({
+            'gs_yjxcD4dGd9yVF6ycGW6bW8gb': { alive: true },
+            'gs_x8HRCtPFT7cHFwwhMWQGWTW4': { alive: false }
+        });
+
+
+        expect(resolveBoardAndGetSnakeStatuses({
+            height: 11,
+            width: 11,
+            snakes: [
+                {
+                    id: 'gs_yjxcD4dGd9yVF6ycGW6bW8gb',
+                    name: 'The Snakening Continues',
+                    latency: '128',
+                    health: 92,
+                    body: [{ x: 9, y: 9 }, { x: 9, y: 8 }, { x: 9, y: 7 }],
+                    head: { x: 9, y: 9 },
+                    length: 3,
+                    shout: 'AHHHHHHHH I\'M A SNAKE',
+                    squad: '',
+                    customizations: { color: '#ac7ef4', head: 'beluga', tail: 'mouse' }
+                },
+                {
+                    id: 'gs_x8HRCtPFT7cHFwwhMWQGWTW4',
+                    name: 'Hungry Bot',
+                    latency: '1',
+                    health: 1,
+                    body: [{ x: 4, y: 4 }, { x: 4, y: 3 }, { x: 3, y: 3 }, { x: 2, y: 3 }],
+                    head: { x: 4, y: 4 },
+                    length: 4,
+                    shout: '',
+                    squad: '',
+                    customizations: { color: '#00cc00', head: 'alligator', tail: 'alligator' }
+                }
+            ],
+            food: [{ x: 10, y: 2 }, { x: 4, y: 4 }, { x: 3, y: 2 }],
+            hazards: []
+        }
+        )).toEqual({
+            'gs_yjxcD4dGd9yVF6ycGW6bW8gb': { alive: true },
+            'gs_x8HRCtPFT7cHFwwhMWQGWTW4': { alive: true }
+        })
     })
+});
+
+
+describe("Moving snake", () => {
+
+    test("Basic moves - right", () => {
+        const snake: Pick<Snake, "body"> = {
+            body: [{ x: 0, y: 0 }, { x: 0, y: 1 }, { x: 0, y: 2 }]
+        }
+        moveSnake(snake as Snake, "right");
+        expect(snake.body).toEqual([{ x: 1, y: 0 }, { x: 0, y: 0 }, { x: 0, y: 1 }])
+    })
+    test("Basic moves - up", () => {
+        const snake: Pick<Snake, "body"> = {
+            body: [{ x: 0, y: 0 }, { x: 1, y: 0 }, { x: 2, y: 0 }]
+        }
+        moveSnake(snake as Snake, "up");
+        expect(snake.body).toEqual([{ x: 0, y: 1 }, { x: 0, y: 0 }, { x: 1, y: 0 }])
+    })
+    test("Basic moves - left", () => {
+        const snake: Pick<Snake, "body"> = {
+            body: [{ x: 3, y: 0 }, { x: 3, y: 1 }, { x: 3, y: 2 }]
+        }
+        moveSnake(snake as Snake, "left");
+        expect(snake.body).toEqual([{ x: 2, y: 0 }, { x: 3, y: 0 }, { x: 3, y: 1 }])
+    })
+    test("Basic moves - down", () => {
+        const snake: Pick<Snake, "body"> = {
+            body: [{ x: 3, y: 3 }, { x: 2, y: 3 }, { x: 1, y: 3 }]
+        }
+        moveSnake(snake as Snake, "down");
+        expect(snake.body).toEqual([{ x: 3, y: 2 }, { x: 3, y: 3 }, { x: 2, y: 3 }])
+    })
+
+    test("Moving down after just ate food (stacked tail)", () => {
+        const snake: Pick<Snake, "body"> = {
+            body: [{ x: 3, y: 3 }, { x: 2, y: 3 }, { x: 1, y: 3 }, { x: 1, y: 3 }]
+        }
+        moveSnake(snake as Snake, "down");
+        expect(snake.body).toEqual([{ x: 3, y: 2 }, { x: 3, y: 3 }, { x: 2, y: 3 }, { x: 1, y: 3 }])
+    })
+});
+
+describe("Perform move on snake then assess outcome", () => {
+    test("basic move. if goes left, will be out of bounds", () => {
+        const board = {
+            height: 11,
+            width: 11,
+            snakes: [
+                {
+                    id: 'gs_yjxcD4dGd9yVF6ycGW6bW8gb',
+                    name: 'The Snakening Continues',
+                    latency: '128',
+                    health: 92,
+                    body: [{ x: 0, y: 9 }, { x: 0, y: 8 }, { x: 0, y: 7 }],
+                    head: { x: 0, y: 9 },
+                    length: 3,
+                    shout: 'AHHHHHHHH I\'M A SNAKE',
+                    squad: '',
+                    customizations: { color: '#ac7ef4', head: 'beluga', tail: 'mouse' }
+                },
+                {
+                    id: 'gs_x8HRCtPFT7cHFwwhMWQGWTW4',
+                    name: 'Hungry Bot',
+                    latency: '1',
+                    health: 94,
+                    body: [{ x: 4, y: 4 }, { x: 4, y: 3 }, { x: 3, y: 3 }, { x: 2, y: 3 }],
+                    head: { x: 4, y: 4 },
+                    length: 4,
+                    shout: '',
+                    squad: '',
+                    customizations: { color: '#00cc00', head: 'alligator', tail: 'alligator' }
+                }
+            ],
+            food: [{ x: 10, y: 2 }, { x: 5, y: 5 }, { x: 3, y: 2 }],
+            hazards: []
+        };
+
+        moveSnake(board.snakes[0], "left");
+        expect(resolveBoardAndGetSnakeStatuses(board)).toEqual({
+            'gs_yjxcD4dGd9yVF6ycGW6bW8gb': { alive: false },
+            'gs_x8HRCtPFT7cHFwwhMWQGWTW4': { alive: true }
+        })
+    });
 })
--- src/game-engine/functions.ts
+++ src/game-engine/functions.ts
@@ -1,3 +1,103 @@
-export function add(a: number, b: number) {
-    return a + b;
+export function moveSnake(snake: Snake, direction: "up" | "down" | "left" | "right") {
+    const head = snake.body[0];
+    let newHead: Snake["body"][number];
+    switch (direction) {
+        case "up":
+            newHead = { x: head.x, y: head.y + 1 };
+            break;
+        case "down":
+            newHead = { x: head.x, y: head.y - 1 };
+            break;
+        case "left":
+            newHead = { x: head.x - 1, y: head.y };
+            break;
+        case "right":
+            newHead = { x: head.x + 1, y: head.y };
+            break;
+    }
+    
+    const length = snake.body.length;
+    const hasStackedTail = snake.body[length - 1].x === snake.body[length - 2].x && snake.body[length - 1].y === snake.body[length - 2].y;
+    const newSnakeBody: Snake["body"] = [newHead];
+    const targetLength = hasStackedTail ? length - 1 : length;
+    for (let i = 1; i < targetLength; i++) {
+        newSnakeBody.push(snake.body[i - 1]);
+    }
+    if (hasStackedTail) { newSnakeBody.push(snake.body[length - 1]); }
+    snake.body = newSnakeBody;
+}
+
+
+
+export function resolveBoardAndGetSnakeStatuses(board: GameBoard) {
+    const snakeStatuses: Record<string, { alive: boolean }> = {};
+    board.snakes.forEach((snake) => {
+        const id = snake.id;
+        if (snakeStatuses[id]?.alive === false) { return }
+        if (isOutOfBounds(snake, board)) {
+            snakeStatuses[id] = { alive: false };
+            return;
+        }
+
+        snakeStatuses[id] = { alive: true };
+        for (let i = 0; i < board.snakes.length; i++) {
+            const otherSnake = board.snakes[i];
+            if (id === otherSnake.id) { continue; }
+            if (snakeStatuses[otherSnake.id]?.alive === false) { continue; }
+
+            const collisionResult = processCollisionCheck(snake, otherSnake);
+            snakeStatuses[id] = collisionResult[id];
+            snakeStatuses[otherSnake.id] = collisionResult[otherSnake.id];
+            if (snakeStatuses[id].alive === false) { break; }
+        }
+
+        snake.health -= 1; //add hazards later
+        if (landedOnFood(snake, board)) { snake.health = 100; }
+        if (snake.health <= 0) { snakeStatuses[id].alive = false }
+    });
+    return snakeStatuses
+}
+
+export function landedOnFood(snake: Snake, board: GameBoard) {
+    const { x, y } = snake.body[0];
+    const foodIndex = board.food.findIndex((food) => food.x === x && food.y === y);
+    return foodIndex > -1
+}
+
+export function isOutOfBounds(snake: Snake, board: GameBoard) {
+    // console.log({ body: snake.body, height: board.height, width: board.width })
+    const { x, y } = snake.body[0];
+    return x < 0 || x >= board.width || y < 0 || y >= board.height
+}
+
+export function processCollisionCheck(snake: Snake, otherSnake: Snake) {
+    const outcome = {
+        [snake.id]: { alive: true },
+        [otherSnake.id]: { alive: true }
+    }
+    const { x, y } = snake.body[0];
+    const otherHead = otherSnake.body[0];
+    if (x === otherHead.x && y === otherHead.y) {
+        if (snake.body.length === otherSnake.body.length) {
+            outcome[snake.id].alive = false;
+            outcome[otherSnake.id].alive = false;
+            return outcome;
+        } else if (snake.body.length > otherSnake.body.length) {
+            outcome[otherSnake.id].alive = false;
+            return outcome;
+        } else {
+            outcome[snake.id].alive = false;
+            return outcome;
+        }
+    }
+
+    for (let i = 1; i < otherSnake.body.length; i++) {
+        const bodyPart = otherSnake.body[i];
+        if (x === bodyPart.x && y === bodyPart.y) {
+            outcome[snake.id].alive = false;
+            return outcome
+        }
+    }
+
+    return outcome
 }
