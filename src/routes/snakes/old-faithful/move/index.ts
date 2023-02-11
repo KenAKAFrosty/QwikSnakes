613a66fa2013bc333acca67d8477bb5ee0bff655
FrostyyPaws <90424167+KenAKAFrosty@users.noreply.github.com>
2023-02-11 18:01:51 -0500

    testing

--- src/routes/snakes/old-faithful/move/index.ts
+++ src/routes/snakes/old-faithful/move/index.ts
@@ -1,4 +1,5 @@
 import { type RequestEvent } from "@builder.io/qwik-city"
+import { moveSnake, resolveBoardAndGetSnakeStatuses } from "../../../../game-engine/functions"
 
 export const onPost = async (event: RequestEvent) => {
     const game: {
@@ -8,6 +9,32 @@ export const onPost = async (event: RequestEvent) => {
         you: Snake
     } = await event.request.json();
     console.log(game);
+
+    const mySnakeIndex = game.board.snakes.findIndex(snake => snake.id === game.you.id);
+
+    const leftScenario = JSON.parse(JSON.stringify(game.board));
+    const rightScenario = JSON.parse(JSON.stringify(game.board));
+    const upScenario = JSON.parse(JSON.stringify(game.board));
+    const downScenario = JSON.parse(JSON.stringify(game.board));
+
+    moveSnake(leftScenario.snakes[mySnakeIndex], "left");
+    moveSnake(rightScenario.snakes[mySnakeIndex], "right");
+    moveSnake(upScenario.snakes[mySnakeIndex], "up");
+    moveSnake(downScenario.snakes[mySnakeIndex], "down");
+
+    const leftStatuses = resolveBoardAndGetSnakeStatuses(leftScenario);
+    const rightStatuses = resolveBoardAndGetSnakeStatuses(rightScenario);
+    const upStatuses = resolveBoardAndGetSnakeStatuses(upScenario);
+    const downStatuses = resolveBoardAndGetSnakeStatuses(downScenario);
+
+    console.log({ 
+        leftStatuses,
+        rightStatuses,
+        upStatuses,
+        downStatuses
+        
+    })
+
     event.headers.set("Content-Type", "application/json");
     event.send(new Response(JSON.stringify({
         move: "up",
