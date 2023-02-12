2c69b595d7723c7b59bf9a6b1dbf1be38fae58ce
FrostyyPaws <90424167+KenAKAFrosty@users.noreply.github.com>
2023-02-12 10:32:36 -0500

    testing log

--- src/routes/snakes/old-faithful/move/index.ts
+++ src/routes/snakes/old-faithful/move/index.ts
@@ -8,14 +8,26 @@ export const onPost = async (event: RequestEvent) => {
         board: GameBoard,
         you: Snake
     } = await event.request.json();
-    console.log(game.board); //let's grab some example board states
 
     //in future will trim board first for performance, but fine for now
     const outcomes = getMoveOutcomes(game.board);
+    const livingOutComesWithTotalLivingSnakeCounts = [];
+    outcomes.forEach(outcome => {
+        if (outcome.statuses[game.you.id].alive === false) { return; }
+
+        let livingSnakeCount = 0;
+        for (const id in outcome.statuses) {
+            if (outcome.statuses[id].alive) { livingSnakeCount++; }
+        }
+        livingOutComesWithTotalLivingSnakeCounts.push({ ...outcome, livingSnakeCount });
+    });
+    console.log(livingOutComesWithTotalLivingSnakeCounts)
+    const livingOutcomes = outcomes.filter(outcome => outcome.statuses[game.you.id].alive);
     // const counts = outcomes.filter(outcome => outcome.gameBoard.snakes.length);
-    const nonDeathMoves = outcomes.filter(outcome => outcome.statuses[game.you.id].alive).map(outcome => {
-        return outcome.gameBoard.snakes.find(snake => snake.id === game.you.id)!.lastMoved
-    })
+    const nonDeathMoves = livingOutcomes.sort((a, b) => Object.keys(b).length - Object.keys(a).length)
+        .map(outcome => {
+            return outcome.gameBoard.snakes.find(snake => snake.id === game.you.id)!.lastMoved
+        })
     const nonDeathMoveCount: Record<Direction, number> = {
         "down": 0,
         "up": 0,
