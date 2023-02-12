dc9c714e03c3d4591073084bacc8c9eea7c0b051
FrostyyPaws <90424167+KenAKAFrosty@users.noreply.github.com>
2023-02-11 22:35:37 -0500

    log focus

--- src/routes/snakes/old-faithful/move/index.ts
+++ src/routes/snakes/old-faithful/move/index.ts
@@ -8,7 +8,7 @@ export const onPost = async (event: RequestEvent) => {
         board: GameBoard,
         you: Snake
     } = await event.request.json();
-    console.log(game.board);
+    // console.log(game.board);
 
     //in future will trim board first for performance, but fine for now
     const outcomes = getMoveOutcomes(game.board);
@@ -16,7 +16,6 @@ export const onPost = async (event: RequestEvent) => {
     const nonDeathMoves = outcomes.filter(outcome => outcome.statuses[game.you.id].alive).map(outcome => {
         return outcome.gameBoard.snakes.find(snake => snake.id === game.you.id)!.lastMoved
     })
-    console.log({ turn: game.turn, nonDeathMoves });
     const moveStillAliveCount: Record<Direction, number> = {
         "down": 0,
         "up": 0,
@@ -26,6 +25,7 @@ export const onPost = async (event: RequestEvent) => {
     nonDeathMoves.forEach(move => {
         moveStillAliveCount[move] += 1;
     });
+    console.log({ turn: game.turn, moveStillAliveCount });
     const bestLifeChance = Math.max(...Object.values(moveStillAliveCount));
     const bestMoves = Object.keys(moveStillAliveCount).filter(move => moveStillAliveCount[move as Direction] === bestLifeChance);
     const chosenMove = bestMoves[Math.floor(Math.random() * bestMoves.length)];
